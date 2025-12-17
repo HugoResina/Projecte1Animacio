e2eb45d7e3712963f89e606c8c705b785f137e06
hresi <hugo.resina.7e7@itb.cat>
2025-12-17 16:18:18 +0100

    polish

--- Assets/Scripts/GoBackScene.cs
+++ Assets/Scripts/GoBackScene.cs
@@ -5,15 +5,26 @@ using System.Collections;
 public class GoBackScene : MonoBehaviour
 {
     public string nombreEscenaDestino = "SampleScene";
+    public GameObject particles;
+    public GameObject particles2;
 
     void Start()
     {
         StartCoroutine(CambiarDespuesDeTiempo());
+        StartCoroutine(DisableParticles());
+
     }
 
     IEnumerator CambiarDespuesDeTiempo()
     {
         yield return new WaitForSeconds(12f);  
         SceneManager.LoadScene(nombreEscenaDestino); 
+    }
+    IEnumerator DisableParticles()
+    {
+        yield return new WaitForSeconds(10f);
+        particles.SetActive(false);
+        particles2.SetActive(false);
+        
     }
 }
